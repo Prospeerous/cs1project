f141d6c91f2b7a8311a069999077022e25f81f5e
Prospeerous <126601092+Prospeerous@users.noreply.github.com>
2024-07-14 19:28:46 +0300

    .

--- database/migrations/2024_06_12_130625_create_products_table.php
+++ database/migrations/2024_06_12_130625_create_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
       
         Schema::create('products', function (Blueprint $table) {
-            $table->pid();
+            $table->id();
            // $table->string('farmer_id')->nullable();
             $table->string('product_name')->nullable();
             $table->string('product_description')->nullable();
